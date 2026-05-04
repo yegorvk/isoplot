bd2f22334d6f0034763fdb62099d2fbf17ada71f
Yegor Vaskonyan <yegor.vaskonyan@gmail.com>
2026-05-04 19:15:40 +0200

    chore(bevy): make dev builds faster
    Bevy is quite heavy, so it is better to link to statically. Luckily, it
    has builtin support for that via dynamic_linking feature flag. However,
    I believe it is a bad idea to hardcode that into `Cargo.toml` since we
    definitely don't want dynamically linked release builds. A possible
    solution, which this commit implements, is to make a custom cargo
    subcommand for dev builds that explicitly passes `-F
    bevy/dynamic_linking` to `cargo run` (i.e., cargo xtask pattern).

---
+++ xtask/src/main.rs
@@ -0,0 +1,59 @@
+use std::{
+    env,
+    path::{Path, PathBuf},
+    process::{self, Command},
+    sync::LazyLock,
+};
+
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[command(subcommand)]
+    action: Action,
+}
+
+#[derive(Debug, Subcommand)]
+enum Action {
+    Dev,
+}
+
+static WORKSPACE_PATH: LazyLock<PathBuf> = LazyLock::new(|| {
+    Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap())
+        .parent()
+        .unwrap()
+        .to_path_buf()
+});
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    match cli.action {
+        Action::Dev => dev()?,
+    };
+
+    Ok(())
+}
+
+fn dev() -> Result<()> {
+    let cargo = env::var("CARGO").unwrap_or_else(|_| "cargo".to_string());
+
+    let status = Command::new(cargo)
+        .current_dir(&*WORKSPACE_PATH)
+        .args(&[
+            "run",
+            "--package",
+            "isoplot-bevy",
+            "-F",
+            "bevy/dynamic_linking",
+        ])
+        .status()
+        .expect("Failed to execute cargo run");
+
+    if !status.success() {
+        process::exit(status.code().unwrap_or(1));
+    }
+
+    Ok(())
+}
